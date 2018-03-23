16687b24121a95fd9479d582fa2d8d73008d8ddc
kevin <kevin@126.com>
2018-03-23 17:59:15 +0800

    add getuser

--- zkapi.py
+++ zkapi.py
@@ -4,6 +4,7 @@
 from win32com.client import Dispatch
 from zkconf import ZK_INFO
 
+# 设置信息
 m_id = 2
 zk = Dispatch("zkemkeeper.ZKEM")
 zk.Connect_Net(ZK_INFO['HOST'], ZK_INFO['PORT'])
@@ -20,7 +21,35 @@ class ZKAPI(object):
     def getReadAllGLogData(self):
         """
         获取所有签到数据
-        :param m_id: 2
+        :return:
+            True， 有数据，
+            1，EnrollNumber/用户编号
+            '***', Password/用户密码
+            'aaa',Name/用户姓名
+            '114',卡号
+            True，Enabled/用户启用
+        """
+        if self.zk.ReadGeneralLogData(self.m_id):
+            alldatas = []
+            while True:
+                data = self.zk.SSR_GetGeneralLogData(self.m_id)
+                rdata = dict()
+                if data[0]:
+                    rdata['is_punch'] = data[0]
+                    rdata['user_id'] = data[1]
+                    rdata['EnrollNumber'] = data[2]
+                    rdata['verifymode'] = data[3]
+                    rdata['create_time'] = '{}-{}-{} {}:{}:{}'.format(data[4], data[5], data[6], data[7], data[8],
+                                                                      data[9])
+                    rdata['desc'] = data[10]
+                    alldatas.append(rdata)
+                else:
+                    break
+            return alldatas
+
+    def getAllUserInfo(self):
+        """
+        读取所有的用户信息
         :return:
             True， 有数据，
             u'x' , 用户id
@@ -34,24 +63,24 @@ class ZKAPI(object):
             37，秒
             0，备用码
         """
-        if self.zk.ReadGeneralLogData(self.m_id):
-            alllogdatas = []
+        if self.zk.GetAllUserInfo(self.m_id):
+            alldatas = []
             while True:
-                data = self.zk.SSR_GetGeneralLogData(self.m_id)
-                logdata = dict()
+                data = self.zk.GetAllUserInfo(self.m_id)
+                rdata = dict()
                 if data[0]:
-                    logdata['is_punch'] = data[0]
-                    logdata['user_id'] = data[1]
-                    logdata['EnrollNumber'] = data[2]
-                    logdata['verifymode'] = data[3]
-                    logdata['create_time'] = '{}-{}-{} {}:{}:{}'.format(data[4], data[5], data[6], data[7], data[8],
-                                                                        data[9])
-                    logdata['desc'] = data[10]
-                    alllogdatas.append(logdata)
+                    rdata['is_punch'] = data[0]
+                    rdata['user_id'] = data[1]
+                    rdata['password'] = data[2]
+                    rdata['username'] = data[3]
+                    rdata['card_id'] = data[4]
+                    rdata['is_active'] = data[5]
+                    alldatas.append(data)
                 else:
                     break
-            return alllogdatas
+            return alldatas
+
 
 if __name__ == '__main__':
     zkapi = ZKAPI(m_id, zk)
-    print(zkapi.getReadAllGLogData())
+    print(zkapi.getAllUserInfo())
