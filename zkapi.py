53bf1d98528d0a65effbc838c19ebb82ca18e53b
kevin <kevin@126.com>
2018-03-24 11:07:33 +0800

    add getuserinfo

--- zkapi.py
+++ zkapi.py
@@ -23,7 +23,7 @@ class ZKAPI(object):
         获取所有签到数据
         :return:
             True， 有数据，
-            1005: user_id
+            1005: EnrollNumber/用户号
             1,verifymode/0 为密码验证，1 为指纹验证，2 为卡验证
             255, 未知
             2015, 年份/ 12，月份/ 21，日期/ 16， 时/ 14，分/ 37，秒
@@ -49,29 +49,30 @@ class ZKAPI(object):
         读取所有的用户信息
         :return:
             True， 有数据，
-            1，EnrollNumber/用户编号
+            1，EnrollNumber/用户号
             'aaa',Name/用户姓名
             '***', Password/用户密码
             '0',Privileg/用户权限，3 为管理员，0 为普通用户
             True，Enabled/用户启用
         """
-        alldatas = []
-        while True:
-            data = self.zk.SSR_GetAllUserInfo(self.m_id)
-            rdata = dict()
-            if data[0]:
-                rdata['user_id'] = data[1]
-                rdata['username'] = data[2]
-                rdata['password'] = data[3]
-                rdata['card_id'] = data[4]
-                rdata['is_active'] = data[5]
-                alldatas.append(rdata)
-            else:
-                break
-        return alldatas
+        if self.zk.ReadAllUserID(self.m_id):
+            alldatas = []
+            while True:
+                data = self.zk.SSR_GetAllUserInfo(self.m_id)
+                rdata = dict()
+                if data[0]:
+                    rdata['user_id'] = data[1]
+                    rdata['username'] = data[2]
+                    rdata['password'] = data[3]
+                    rdata['card_id'] = data[4]
+                    rdata['is_active'] = data[5]
+                    alldatas.append(rdata)
+                else:
+                    break
+            return alldatas
 
 
 if __name__ == '__main__':
     zkapi = ZKAPI(m_id, zk)
-    print(zkapi.getReadAllGLogData())
+    print(zkapi.getAllUserInfo())
     zk.Disconnect()
