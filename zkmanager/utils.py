2d46ad805e7ed70a1a9dd3afeb7a0c16e886c0ce
kevin <kevin@126.com>
2018-04-02 15:37:23 +0800

    add get allday punch

--- zkmanager/utils.py
+++ zkmanager/utils.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # author: itimor
 
-from datetime import timedelta
+from datetime import timedelta, date
+import calendar
+import requests
 
 
 def diff_times_in_seconds(t1, t2):
@@ -11,3 +13,16 @@ def diff_times_in_seconds(t1, t2):
     t2_secs = s2 + 60 * (m2 + 60 * h2)
     tc = str(timedelta(seconds=(t2_secs - t1_secs)))
     return tc
+
+
+def getallday(year, smouth, emouth):
+    url = 'http://127.0.0.1:9000/api/getpunchs/'
+    for mouth in range(smouth, emouth + 1):
+        week, monthRange = calendar.monthrange(year, mouth)
+        for day in range(1, monthRange + 1):
+            sday = date(year, mouth, day).strftime('%Y-%m-%d')
+            requests.get(url + sday + '/')
+    return
+
+if __name__ == '__main__':
+    print(getallday(2018, 1, 3))
