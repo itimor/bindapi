952abd45490da8505358bfbd112947b685d775eb
kevin <kevin@126.com>
2018-04-19 10:37:36 +0800

    fix aaa

--- utils/a.py
+++ utils/a.py
@@ -11,21 +11,22 @@ def diffdns(allurl):
     for url in allurls:
         uu = 'http://127.0.0.1:8888/api/domainstatus/?domain='
         urlinfos = json.loads(requests.get(uu + url).text)
-        ss = ee = []
+        ss = []
+        ee = []
         for info in urlinfos:
             if info['status']:
                 ss.append(info['node'])
             else:
                 ee.append(info['node'])
-
         result = dict()
         result['node_count'] = len(urlinfos)
+        result['error_node'] = ee
+
         if len(ss) > result['node_count']/2:
             result['status'] = True
-            result['error_node'] = ee
         else:
             result['status'] = False
-            result['error_node'] = ee
+
         oo.append({url: result})
     return oo
 
