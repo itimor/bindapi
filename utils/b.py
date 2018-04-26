584e78fae552f2d24ca8028987565ccb0086cfad
kevin <kevin@126.com>
2018-04-26 10:43:42 +0800

    add

--- utils/b.py
+++ utils/b.py
@@ -2,23 +2,35 @@
 # author: itimor
 
 
-class Solution:
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        used = {}
-        max_length = start = 0
-        for i, c in enumerate(s):
-            if c in used and start <= used[c]:
-                start = used[c] + 1
-            else:
-                max_length = max(max_length, i - start + 1)
-            used[c] = i
-        return max_length
+import time
+import threading
 
 
-a = [2, 4, 3]
-b = [5, 6, 4]
-print(Solution().lengthOfLongestSubstring("abcabcbdab"))
+class MyThread(threading.Thread):
+    def run(self):
+        for i in range(5):
+            print('thread {}, @number: {}'.format(self.name, i))
+            time.sleep(2)
+
+
+def main():
+    print("Start main threading")
+    # 创建三个线程
+    threads = [MyThread() for i in range(3)]
+    # 启动三个线程
+    for t in threads:
+        t.start()
+
+    # 一次让新创建的线程执行 join
+    for t in threads:
+        t.join()
+
+    print("End Main threading")
+
+
+if __name__ == '__main__':
+    s_time = time.time()
+    main()
+    # MyThread().run()
+    e_time = time.time()
+    print(e_time - s_time)
--- utils/client_tan.py
+++ utils/client_tan.py
@@ -3,28 +3,64 @@
 
 import requests
 import json
+import time
+import threading
+import sys
 
-node = 'node01'
+if sys.version_info < (3, 4):
+    from Queue import Queue
+else:
+    from queue import Queue
 
-bind_api_url = 'http://118.193.136.206:8000/api/'
-tan_page = '/favicon.ico'
-post_url = bind_api_url + 'domainstatus/'
-alldomain_url = bind_api_url + 'alldomains/'
 
-domains = requests.get(alldomain_url)
+class ThreadUrl(threading.Thread):
 
-for domain in json.loads(domains.text):
-    url = 'http://' + domain + tan_page
-    html = requests.get(url, verify=False)
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
 
-    post_message = {
-        'node': node,
-        'domain': domain,
-    }
+    def run(self):
+        pass
 
-    if html.status_code == '200':
-        post_message['status'] = True
-    else:
-        post_message['status'] = False
 
-    requests.post(post_url, data=post_message)
+class Tan(object):
+    def __init__(self, node, tan_page):
+        self.node = node
+        self.bind_api_url = 'http://118.193.136.206:8000/api/'
+        self.tan_page = tan_page
+        self.tanpost_uri = 'domainstatus/'
+        self.alldomain_uri = 'alldomains/'
+
+    def get_domains(self):
+        req = requests.get(self.bind_api_url + self.alldomain_uri)
+        return json.loads(req.text)
+
+
+if __name__ == '__main__':
+    node = 'node01'
+    tan_page = '/favicon.ico'
+    tan = Tan(node, tan_page)
+    domains = tan.get_domains()
+
+    threads = []
+    for domain in domains:
+        url = 'http://' + domain + tan.tan_page
+        html = requests.get(url, verify=False)
+
+        post_message = {
+            'node': node,
+            'domain': domain,
+        }
+
+        if html.status_code == '200':
+            post_message['status'] = True
+        else:
+            post_message['status'] = False
+
+        # requests.post(tan.bind_api_url + tan.tanpost_uri, data=post_message)
+        t = threading.Thread(target=requests.post(tan.bind_api_url + tan.tanpost_uri, data=post_message))
+        threads.append(t)
+
+        time.sleep(20)
+        for thr in threads:
+            thr.start()
