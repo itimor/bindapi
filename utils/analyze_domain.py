5459e8e15e020c46568fd15d79b2cc3b33ba8d13
kevin <kevin@126.com>
2018-04-27 11:21:52 +0800

    auto change ip

--- utils/analyze_domain.py
+++ utils/analyze_domain.py
@@ -15,12 +15,11 @@ def diffdns(alldomains):
         cur_date = '{}-{}-{}'.format(d.year, d.month, d.day)
         cur_time = '{}:{}:{}'.format(d.hour, d.minute, d.second)
         create_time = '{}:{}:{}'.format(d10.hour, d10.minute, d10.second)
-        uu = 'http://118.193.136.206:8000/api/domainstatus/?domain={}&create_time_0={}&create_time_1={}'.format(
-            domain, cur_date, create_time, cur_time
-        )
+        uu = 'http://118.193.136.206:8000/api/domainstatus/?domain={}&create_time_0={}&create_time_1={}'.format(domain, cur_date, create_time, cur_time)
+        # uu = 'http://118.193.136.206:8000/api/domainstatus/?domain={}'.format(domain)
         urlinfos = json.loads(requests.get(uu).text)
-        if len(urlinfos) < 0:
-            return {'error': 'status is empty!'}
+        if not len(urlinfos):
+            break
 
         ss = []
         ee = []
@@ -38,6 +37,25 @@ def diffdns(alldomains):
             result['status'] = True
         else:
             result['status'] = False
+
+            # 自动切换ip
+            record_url = 'http://oms.tb-gaming.local/api/dnsrecords/'
+            # record_url = 'http://127.0.0.1:8000/api/dnsrecords/'
+
+            x = domain.split('.')
+            domainname = '{}.{}'.format(x[1], x[2])
+            recordname = x[0]
+
+            recorddata = json.loads(
+                requests.get('{}?domain__name={}&name={}'.format(record_url, domainname, recordname)).text)[0]
+
+            if recorddata['value2']:
+                recorddata['value'], recorddata['value2'] = recorddata['value2'], recorddata['value']
+                put_url = '{}{}/'.format(record_url, recorddata['id'])
+                requests.put(put_url, data=recorddata)
+                print('ip已自动更换')
+            else:
+                print('没有设置备用ip')
 
         oo.append(result)
     return oo
